4d8ca613b3f3e7b82806872718ef7dd568b4a714
Jordan Mance <mancej@snagajob.com>
2020-07-17 20:24:46 -0400

    Converting ReplictionType back to Enum

--- src/figgy/models/replication_config.py
+++ src/figgy/models/replication_config.py
@@ -8,15 +8,13 @@ from typing import Dict, List
 from figgy.utils.utils import *
 
 
-class ReplicationType:
+class ReplicationType(Enum):
     """
     Represents the various types of replication configs that are valid for storing into our dynamodb
-    `config-replication` table
+    `service-config-replication` table
     """
-
-    def __init__(self, type: str):
-        Utils.validate(type in REPL_TYPES, f"Type: {type} must be one of: {REPL_TYPES}")
-        self.type = type
+    APP = REPL_TYPE_APP
+    MERGE = REPL_TYPE_MERGE
 
 
 class ReplicationConfig:
@@ -30,7 +28,7 @@ class ReplicationConfig:
         self.run_env = run_env.env
         self.namespace = namespace
         self.source = source
-        self.type: str = type.type
+        self.type: str = type.value
         self.user = user
 
         if user is None:
