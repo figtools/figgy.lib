c114779a72063d34b3f8bbce57dc46e21a6f93d9
Jordan Mance <mancej@snagajob.com>
2020-07-19 15:57:34 -0400

    Fixing an issue with parameter restores across multipages

--- src/figgy/data/dao/config.py
+++ src/figgy/data/dao/config.py
@@ -54,7 +54,7 @@ class ConfigDao:
         if start_key:
             result = self._audit_table.scan(FilterExpression=filter_exp, ExclusiveStartKey=start_key)
         else:
-            result = self._audit_table.scan(FilterExpression=filter_exp, )
+            result = self._audit_table.scan(FilterExpression=filter_exp)
 
         items: List = result["Items"] if result["Items"] else []
 
@@ -69,7 +69,7 @@ class ConfigDao:
         # Convert to RestoreConfig model then sort list chronologically by timestamp
         return results
 
-    def get_all_parameter_history(self, ps_time: datetime.datetime, ps_prefix: str, start_key=None) -> List[Dict]:
+    def get_all_parameter_history(self, ps_time: datetime.datetime, ps_prefix: str) -> List[RestoreConfig]:
         """
         Scans in DynamoDb only do 1MB at at time. For large tables, we need to tell dynamo to KEEP_SCANNING. After each
         result set is returned, we need to inform dynamo to keep scanning to find ALL results across the full table. This
@@ -78,26 +78,19 @@ class ConfigDao:
             ps_time: Time up to which parameter history should be returned.
             ps_prefix: e.g. /shared/some/prefix - Prefix to query under
             start_key: LastEvaluatedKey returned in scan results. Lets you konw if there is more scanning that can be done.
-        Returns: List[Dict] of items returned from AWS API
+        Returns: List[RestoreConfig] mapped from histories
         """
         time_end = Decimal(ps_time.timestamp() * 1000)
         filter_exp = Key(AUDIT_TIME_KEY_NAME).lt(time_end) & Key(AUDIT_PARAMETER_KEY_NAME).begins_with(ps_prefix)
-        results = []
 
-        if start_key:
-            result = self._audit_table.scan(FilterExpression=filter_exp, ExclusiveStartKey=start_key)
-        else:
-            result = self._audit_table.scan(FilterExpression=filter_exp)
+        result = self._audit_table.scan(FilterExpression=filter_exp)
+        items = result.get('Items', [])
 
-        if 'LastEvaluatedKey' in result:
-            results.append(self.get_all_parameter_history(ps_time, ps_prefix, start_key=result['LastEvaluatedKey']))
+        while 'LastEvaluatedKey' in result:
+            result = self._audit_table.scan(FilterExpression=filter_exp, ExclusiveStartKey=result['LastEvaluatedKey'])
+            items = items + result.get('Items', [])
 
-        results.append(result)  # Add results from this query to list of lists
-
-        results_list = list(map(lambda x: x['Items'], results))  # extract all the items
-        result['Items'] = [item for sublist in results_list for item in sublist]  # Merge sublists to one list
-
-        return result
+        return RestoreConfig.convert_to_model(result.get('Items', []))
 
     def get_parameter_history_before_time(self, ps_time: datetime.datetime, ps_prefix: str) -> PSHistory:
         """
@@ -110,9 +103,7 @@ class ConfigDao:
 
         """
 
-        result = self.get_all_parameter_history(ps_time, ps_prefix)
-        items: List = result.get('Items', [])
-        restore_cfgs: List[RestoreConfig] = RestoreConfig.convert_to_model(items)
+        restore_cfgs: List[RestoreConfig] = self.get_all_parameter_history(ps_time, ps_prefix)
 
         ps_histories: Dict[str, ParameterHistory] = {}
         for cfg in restore_cfgs:
@@ -123,7 +114,7 @@ class ConfigDao:
 
         return PSHistory(list(ps_histories.values()))
 
-    def get_parameter_restore_range(self, ps_time: datetime.datetime, ps_prefix: str, start_key: str = None) \
+    def get_parameter_restore_range(self, ps_time: datetime.datetime, ps_prefix: str) \
             -> List[RestoreConfig]:
         """
         :param ps_time:  int -> datetime to query dynamo timestamp from
@@ -136,15 +127,16 @@ class ConfigDao:
         filter_exp = Key(AUDIT_TIME_KEY_NAME).lt(time_end) & Key(AUDIT_PARAMETER_KEY_NAME).begins_with(ps_prefix) \
                      & Attr(AUDIT_ACTION_ATTR_NAME).eq(SSM_PUT)
 
-        if start_key:
-            result = self._audit_table.scan(FilterExpression=filter_exp, ExclusiveStartKey=start_key)
-        else:
-            result = self._audit_table.scan(FilterExpression=filter_exp)
+        result = self._audit_table.scan(FilterExpression=filter_exp)
+        items = list(filter(lambda x: x["action"] == "PutParameter", result.get('Items', [])))
 
-        items: List = result.get('Items', [])
-        items = list(filter(lambda x: x["action"] == "PutParameter", items))
+        while 'LastEvaluatedKey' in result:
+            result = self._audit_table.scan(FilterExpression=filter_exp, ExclusiveStartKey=result['LastEvaluatedKey'])
+            items = items + list(filter(lambda x: x["action"] == "PutParameter", result.get('Items', [])))
 
         return sorted(RestoreConfig.convert_to_model(items), key=lambda x: x.ps_time, reverse=True)
+
+
 
     def get_all_configs(self, namespace: str, start_key: str = None) -> List[ReplicationConfig]:
         """
@@ -158,27 +150,13 @@ class ConfigDao:
 
         """
         filter_exp = Attr(REPL_NAMESPACE_ATTR_NAME).eq(namespace)
-        if start_key:
+
+        result = self._config_repl_table.scan(FilterExpression=filter_exp)
+        configs = [self.__cfg_from_boto_item(item) for item in result.get('Items', [])]
+
+        while 'LastEvaluatedKey' in result:
             result = self._config_repl_table.scan(FilterExpression=filter_exp, ExclusiveStartKey=start_key)
-        else:
-            result = self._config_repl_table.scan(FilterExpression=filter_exp)
-
-        items = result["Items"] if result["Items"] else None
-
-        configs = []
-        if items is not None:
-            for item in items:
-                repl_config = ReplicationConfig(
-                    item[REPL_DEST_KEY_NAME],
-                    RunEnv(item.get(REPL_RUN_ENV_KEY_NAME, "unknown")),
-                    item[REPL_NAMESPACE_ATTR_NAME],
-                    item[REPL_SOURCE_ATTR_NAME],
-                    ReplicationType(item[REPL_TYPE_ATTR_NAME]),
-                )
-                configs.append(repl_config)
-
-        if 'LastEvaluatedKey' in result:
-            configs = configs + self.get_all_configs(namespace, start_key=result['LastEvaluatedKey'])
+            configs = configs + [self.__cfg_from_boto_item(item) for item in result.get('Items', [])]
 
         return configs
 
@@ -192,12 +170,12 @@ class ConfigDao:
         start_time = time.time()
         filter_exp = Attr(REPL_SOURCE_ATTR_NAME).eq(source)
         response = self._config_repl_table.scan(FilterExpression=filter_exp)
-        configs: List[ReplicationConfig] = self._map_results(response)
+        configs: List[ReplicationConfig] = self.__map_results(response)
 
         while 'LastEvaluatedKey' in response:
             response = self._config_repl_table.scan(FilterExpression=filter_exp,
                                                     ExclusiveStartKey=response['LastEvaluatedKey'])
-            configs = configs + self._map_results(response)
+            configs = configs + self.__map_results(response)
 
         log.info(f"Returning {len(configs)} parameter names from dynamo cache after "
                  f"{time.time() - start_time} seconds.")
@@ -251,30 +229,6 @@ class ConfigDao:
                 item[key] = Decimal(f"{config.props[key]}")
 
         self._config_repl_table.put_item(Item=item)
-
-    def _map_results(self, result: dict) -> List[ReplicationConfig]:
-        """
-        Takes a DDB Result object with a single result and maps it into a replication config
-        Args:
-            result: DDB boto3 result obj
-
-        Returns: ReplicationConfig if result is found, else and empty list
-
-        """
-        repl_cfgs = []
-        if "Items" in result and len(result["Items"]) > 0:
-            for item in result["Items"]:
-                config = ReplicationConfig(
-                    item[REPL_DEST_KEY_NAME],
-                    RunEnv(item.get(REPL_RUN_ENV_KEY_NAME, "unknown")),
-                    item[REPL_NAMESPACE_ATTR_NAME],
-                    item[REPL_SOURCE_ATTR_NAME],
-                    ReplicationType(item[REPL_TYPE_ATTR_NAME]),
-                    user=item[REPL_USER_ATTR_NAME],
-                )
-                repl_cfgs.append(config)
-
-        return repl_cfgs
 
     def get_audit_logs(self, ps_name: str) -> List[AuditLog]:
         """
@@ -378,3 +332,29 @@ class ConfigDao:
         }
 
         self._cache_table.put_item(Item=item)
+
+    ## Mapping and utilities Todo: Should be moved / refactored
+    def __cfg_from_boto_item(self, boto_item: Dict) -> ReplicationConfig:
+        return ReplicationConfig(
+            boto_item[REPL_DEST_KEY_NAME],
+            RunEnv(boto_item.get(REPL_RUN_ENV_KEY_NAME, "unknown")),
+            boto_item[REPL_NAMESPACE_ATTR_NAME],
+            boto_item[REPL_SOURCE_ATTR_NAME],
+            ReplicationType(boto_item[REPL_TYPE_ATTR_NAME]),
+        )
+
+    def __map_results(self, result: dict) -> List[ReplicationConfig]:
+        """
+        Takes a DDB Result object with a single result and maps it into a replication config
+        Args:
+            result: DDB boto3 result obj
+
+        Returns: ReplicationConfig if result is found, else and empty list
+
+        """
+        repl_cfgs = []
+        if "Items" in result and len(result["Items"]) > 0:
+            for item in result["Items"]:
+                repl_cfgs.append(self.__cfg_from_boto_item(item))
+
+        return repl_cfgs
