ee9f8f51155b2ca9d75fe9baaebd9687c72e5f57
Jordan Mance <mancej@snagajob.com>
2021-04-13 19:44:50 -0400

    Adding support for disabling prefixes in cache returns

--- src/figgy/data/dao/config.py
+++ src/figgy/data/dao/config.py
@@ -1,6 +1,6 @@
 import logging
 import time
-from typing import Set
+from typing import Set, List
 
 from boto3.dynamodb.conditions import Attr
 
@@ -21,16 +21,24 @@ class ConfigDao:
         self._dynamo_resource = dynamo_resource
         self._cache_table = self._dynamo_resource.Table(CACHE_TABLE_NAME)
 
-    def get_all_config_names(self, prefix: str = None, one_level: bool = False, start_key: str = None) -> Set[str]:
+    def get_all_config_names(self, prefix: str = None,
+                             exclude_prefixes=None,
+                             one_level: bool = False,
+                             start_key: str = None) -> Set[str]:
         """
         Retrieve all key names from the Dynamo DB config-cache table in each account. Much more efficient than
         querying SSM directly.
         Args:
             start_key: Optional: Is used for recursive paginated lookups to get the full data set. This should not
             be passed in by the user.
+            exclude_prefixes: configs with these prefixes will be excluded from results
         Returns:
 
         """
+
+        if exclude_prefixes is None:
+            exclude_prefixes = ['/figgy']
+
         start_time = time.time()
         if start_key:
             log.info(f"Recursively scanning with start key: {start_key}")
@@ -47,25 +55,34 @@ class ConfigDao:
         if prefix:
             configs = set(filter(lambda x: x.startswith(prefix), configs))
 
+        if exclude_prefixes:
+            for excluded_prefix in exclude_prefixes:
+                configs = set(filter(lambda x: not x.name.startswith(excluded_prefix), configs))
+
         if one_level:
             configs = set(filter(lambda x: len(x.split('/')) == len(prefix.split('/')) + 1, configs))
 
         if 'LastEvaluatedKey' in result:
-            configs = configs | self.get_all_config_names(prefix, one_level, start_key=result['LastEvaluatedKey'])
+            configs = configs | self.get_all_config_names(prefix, exclude_prefix, one_level,
+                                                          start_key=result['LastEvaluatedKey'])
 
         log.info(
             f"Returning config names from dynamo cache after: {time.time() - start_time} "
             f"seconds with {len(configs)} configs.")
         return configs
 
-    def get_config_names_after(self, millis_since_epoch: int) -> Set[ConfigItem]:
+    def get_config_names_after(self, millis_since_epoch: int, exclude_prefixes=None) -> Set[ConfigItem]:
         """
         Retrieve all key names from the Dynamo DB config-cache table in each account. Much more efficient than
         querying SSM directly.
         Args:
             millis_since_epoch: milliseconds in epoch to lookup config names from cache after
+            exclude_prefixes: configs with these prefixes will be excluded from results
         Returns: Set[str] -> configs that have been added to cache table after millis_since_epoch
         """
+
+        if exclude_prefixes is None:
+            exclude_prefixes = ['/figgy']
 
         request = {
             'attribute_names': {
@@ -89,8 +106,13 @@ class ConfigDao:
             items = items + response.get('Items', [])
 
         configs: Set[ConfigItem] = set()
+
         for item in items:
             configs.add(ConfigItem(**item))
+
+        if exclude_prefixes:
+            for excluded_prefix in exclude_prefixes:
+                configs = set(filter(lambda x: not x.name.startswith(excluded_prefix), configs))
 
         log.info(f"Returning {len(configs)} parameter names from dynamo cache after time: [{millis_since_epoch}] in "
                  f"{time.time() - start_time} seconds.")
